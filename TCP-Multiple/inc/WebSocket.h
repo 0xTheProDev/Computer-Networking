49ec05e53ebb812d1b16b7f48a9cc402fe75963c
Progyan Bhattacharya <progyanb@acm.org>
2018-04-12 11:30:23 +0530

    Lab: 06-04-2018: Concurent TCP Connection with Server in Chat application

---
+++ TCP-Multiple/inc/WebSocket.h
@@ -0,0 +1,80 @@
+/**
+ * Copyright (c) 2018 Progyan Bhattacharya <progyanb@acm.org>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/* WebSocket.h: Header file written for experimenting with Socket Programming */
+
+#ifndef __WebSocket_h
+#define __WebSocket_h
+
+#define _XOPEN_SOURCE
+
+#ifndef __arpa_inet_h
+    #include <arpa/inet.h>
+#endif
+#ifndef __signal_h
+    #include <signal.h>
+#endif
+#ifndef __stdio_h
+    #include <stdio.h>
+#endif
+#ifndef __stdbool_h
+    #include <stdbool.h>
+#endif
+#ifndef __stdlib_h
+    #include <stdlib.h>
+#endif
+#ifndef __string_h
+    #include <string.h>
+#endif
+#ifndef __strings_h
+    #include <strings.h>
+#endif
+#ifndef __sys_socket_h
+    #include <sys/socket.h>
+#endif
+#ifndef __sys_types_h
+    #include <sys/types.h>
+#endif
+#ifndef __unistd_h
+    #include <unistd.h>
+#endif
+
+#ifndef SOCKET_IP
+    #define SOCKET_IP "127.0.0.1"
+#endif
+#ifndef SOCKET_PORT
+    #define SOCKET_PORT 8080
+#endif
+#ifndef SOCKET_PROTO
+    #define SOCKET_PROTO 0
+#endif
+#ifndef MSG_SIZE
+    #define MSG_SIZE 128
+#endif
+#ifndef MSG_FLAG
+    #define MSG_FLAG 0
+#endif
+#ifndef MAX_CONN
+    #define MAX_CONN 10
+#endif
+
+#endif
---
+++ TCP-Multiple/src/Server.c
@@ -0,0 +1,105 @@
+/**
+ * Copyright (c) 2018 Progyan Bhattacharya <progyanb@acm.org>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/* Server for Concurrent Chat Program */
+
+#include <WebSocket.h>
+
+int main(int argc, const char* argv[]) {
+    if (argc > 2) {
+        fprintf(stderr, "Usage: %s [queueSize]\n", argv[0]);
+        return -1;
+    }
+    const int queueSize = argc == 2 ? atoi(argv[1]) : 1024;
+    struct sockaddr_in server, client;
+    socklen_t client_addr_len = sizeof(client);
+    memset(&server, 0, sizeof(server));
+    memset(&client, 0, sizeof(client));
+    server.sin_addr.s_addr = htonl(INADDR_ANY);
+    server.sin_family = AF_INET;
+    server.sin_port = htons(SOCKET_PORT);
+    int sfd = socket(AF_INET, SOCK_STREAM, SOCKET_PROTO);
+    if (sfd == -1) {
+        fprintf(stderr, "Error: Failed to create a server socket\n");
+        return -2;
+    }
+    if (bind(sfd, (struct sockaddr*) &server, sizeof(server))) {
+        fprintf(stderr, "Failed to bind server socket\n");
+        close(sfd);
+        return -3;
+    }
+    if (listen(sfd, queueSize)) {
+        fprintf(stderr, "Failed to start server socket\n");
+        close(sfd);
+        return -4;
+    }
+    printf("Server started at %s:%d\n", inet_ntoa(server.sin_addr), SOCKET_PORT);
+    while (true) {
+        int cfd = accept(sfd, (struct sockaddr *) &client, &client_addr_len);
+        if (cfd < 0) {
+            fprintf(stderr, "Failed to accept client connection\n");
+            close(sfd);
+            return -5;
+        }
+        printf("Established connection with client %s\n", inet_ntoa(client.sin_addr));
+        pid_t pid = fork();
+        if (pid == 0) {
+            char sndBfr[MSG_SIZE], rcvBfr[MSG_SIZE];
+            while (true) {
+                memset(sndBfr, 0, sizeof(sndBfr));
+                memset(rcvBfr, 0, sizeof(rcvBfr));
+                if (recv(cfd, rcvBfr, sizeof(rcvBfr), MSG_FLAG) == -1) {
+                    fprintf(stderr, "Failed to receive message from %s\n", inet_ntoa(client.sin_addr));
+                    close(cfd);
+                    close(sfd);
+                    return -6;
+                }
+                printf("Received message from %s\n\t%s\n", inet_ntoa(client.sin_addr), rcvBfr);
+                if (strcasecmp(rcvBfr, "exit") == 0) break;
+                printf("Enter the message you want to send:\n");
+                scanf("%s", sndBfr);
+                if (send(cfd, sndBfr, sizeof(sndBfr), MSG_FLAG) == -1) {
+                    fprintf(stderr, "Failed to send message to %s\n", inet_ntoa(client.sin_addr));
+                    close(cfd);
+                    close(sfd);
+                    return -7;
+                }
+                printf("Sent message to %s\n\t%s\n", inet_ntoa(client.sin_addr), sndBfr);
+                if (strcasecmp(sndBfr, "exit") == 0) break;
+            }
+            if (close(cfd)) {
+                fprintf(stderr, "Failed to close client socket\n");
+                return -8;
+            }
+            printf("Closed connection with client %s\n", inet_ntoa(client.sin_addr));
+        } else if (pid == -1) {
+            fprintf(stderr, "Failed to create new process\n");
+            return -9;
+        }
+    }
+    if (close(sfd)) {
+        fprintf(stderr, "Failed to close server socket\n");
+        return -10;
+    }
+    printf("Closed connection with server %s\n", inet_ntoa(server.sin_addr));
+    return 0;
+}
